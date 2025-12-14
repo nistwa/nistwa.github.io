f8680325f1086ba65d9e8c2bb5aa106aaeef52b7
beratedis <125279056+beratedis@users.noreply.github.com>
2025-12-14 15:42:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Web sitesi yüklendi!");
+const btn = document.getElementById("btn");
+const statusText = document.getElementById("status");
+
+
+btn.addEventListener("click", () => {
+statusText.innerText = "Sistem aktif. AKANA ROBOTICS hazır 💪";
+});
